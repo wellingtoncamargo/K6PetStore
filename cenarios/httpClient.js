1fa520b61b41010522c3f507200038092af7c42e
Wellington Camargo <wellingtonsc@hotmail.com>
2025-03-17 15:53:36 -0300

    Criando http pets

--- cenarios/httpClient.js
+++ cenarios/httpClient.js
@@ -1,9 +1,11 @@
 import http from 'k6/http';
+import * as body from '../utils/BodyRequest.js'
 import { sleep, fail, check } from 'k6';
 import {Trend, Rate, Counter} from 'k6/metrics';
 
 const headers = {
     'accept': 'application/json',
+    'Content-Type': 'application/json',
     'api_key':'special-key'
 };
 
@@ -13,7 +15,7 @@ const customTrend = new Trend('custom_waiting_time');
 const requestCount = new Counter('custom_request_count');
 const failureRate = new Rate('custom_failure_rate');
 
-export function gatPetsbyStatus(status){ //pending
+export function getPetsbyStatus(status){ //pending
     const lista = ['pending','sold', 'available']
     
     const res = http.get(`https://petstore.swagger.io/v2/pet/findByStatus?status=${lista.includes(status)? status : fail('Status inexistente ou não informado!')}`)
@@ -27,6 +29,65 @@ export function gatPetsbyStatus(status){ //pending
     return res.body != null? JSON.parse(res.body): fail('Status inexistente ou não informado!')
 }
 
+export function getPetsbyId(id){ 
+    
+    const res = http.get(`https://petstore.swagger.io/v2/pet/${id}`)
+
+    check(res,{
+        "Consulta por id retornado com sucesso": (r) => r.status === statusGet
+    })
+    customTrend.add(res.timings.waiting);
+    requestCount.add(1);
+    failureRate.add(res.status !== statusGet);
+    return JSON.parse(res.body)
+}
+
+export function postNewPet(){
+        
+    const res = http.post(`https://petstore.swagger.io/v2/pet`,body.novoPet(), { headers })
+
+    check(res,{
+        "Pet cadastrado com sucesso": (r) => r.status === statusGet
+    })
+    customTrend.add(res.timings.waiting);
+    requestCount.add(1);
+    failureRate.add(res.status !== statusGet);
+    return JSON.parse(res.body)
+}
+
+export function putPet(putBody){
+        
+    const res = http.put(`https://petstore.swagger.io/v2/pet`, JSON.stringify(putBody), { headers })
+
+    check(res,{
+        "Cadastro alterado com sucesso": (r) => r.status === statusGet
+    })
+    customTrend.add(res.timings.waiting);
+    requestCount.add(1);
+    failureRate.add(res.status !== statusGet);
+    return JSON.parse(res.body)
+}
+
+export function deletePetbyId(id){ 
+    
+    const res = http.del(`https://petstore.swagger.io/v2/pet/${id}`)
+
+    check(res,{
+        "Apagando pet por id retornado com sucesso": (r) => r.status === statusGet
+    })
+    customTrend.add(res.timings.waiting);
+    requestCount.add(1);
+    failureRate.add(res.status !== statusGet);
+    return JSON.parse(res.body)
+}
+
+
 export default function () {
-    console.info(gatPetsbyStatus('pending'))
+    const post = postNewPet()
+    console.info(post)
+    post.category.name = 'Cachorro'
+    console.info(post)
+    console.info(putPet(post))
+    console.info(deletePetbyId(post.id))
+    
 }
--- utils/BodyRequest.js
+++ utils/BodyRequest.js
@@ -1,177 +1,25 @@
 import { uuidv4 } from 'https://jslib.k6.io/k6-utils/1.4.0/index.js';
+import faker from "k6/x/faker";
 const novaData = new Date()
 const novoUUID = uuidv4()
 
-export function bodyCreditoParcelado(customer, parcela, valor, bandeira) {
-  
-  novaData.setUTCHours(novaData.getHours())
-  return {
-    "transaction_code": novoUUID.replaceAll('-', '').toUpperCase(),
-    "creation_date": novaData.toISOString().substring(0, 19),
-    "value": valor,
-    "establishment": 1050,
-    "original_value": valor,
-    "installment_quantity": parcela,
-    "original_status": "PAID",
-    "customer_code": customer,
-    "cod_authorization": 123456,
-    "acquirer": "PAGSEGURO",
-    "card_brand": bandeira.toUpperCase(),
-    "capture_type": "E",
-    "ip": "127.0.0.1",
-    "port": 12345,
-    "card_hash_df": "HASHDF",
-    "bin": "471672",
-    "holder": "0637",
-    "pan_mask": "471672******0637",
-    "pan_mask_full": "471672******0637",
-    "user_code": "123456",
-    "device": {
-      "model": "samsung SM-J105B",
-      "imei": "IMEI",
-      "device_id": "DEVICEID",
-      "device_type": "C",
-      "reader_serial_number": "SERIALNUMBER",
-      "reader_model": "PAX_S920_BGW"
+export function novoPet(){
+  return JSON.stringify({
+    "id": 1,
+    "category": {
+      "id": 1,
+      "name": "Chachorro"
     },
-    "mobile_application_id": "34335",
-    "ksn": "629949804109e9600122",
-    "nsu_sitef": "123654",
-    "nsu_host": "976431",
-    "payment_type": "C",
-    "firmware": "2.4.123",
-    "shared_library": "001.38 191125",
-    "latitude": "-10.0",
-    "longitude": "-20.0",
-    "company": "T1234567890",
-    "user_reference": "666667",
-    "simcard_serial": "81234567890",
-    "terminal": "AP392170",
-    "switch_response": {
-      "switch_cod_transaction": novoUUID.replaceAll('-', '').toUpperCase(),
-      "switch_response_code": 0,
-      "switch_response_message": "TRANSACAO AUTORIZADA",
-      "switch_response_code_host": 0,
-      "switch_response_date_host": novaData.toISOString().substring(0, 19),
-      "switch_status": 0,
-      "switch_creation_date": novaData.toISOString().substring(0, 19)
-    },
-    "cne": "0005/88694580",
-    "arqc": "ARQC",
-    "aid": "A0000005372010",
-    "aid_name": `CREDITO ${bandeira.toUpperCase()}`,
-    "gateway": "SWITCH",
-    "authentication": 1,
-    "capture_mode": "C",
-    "capture_method": "",
-    "initialization_mode": 1,
-    "acquirer_category": 1,
-    "integrator_code": "INTEGRATOR CODE",
-    "soft_descriptor": "Pag*SoftDescriptor",
-    "user_application_id": "6814944"
-  }
-}
-
-export function bodyAlteraEscrowUnique(value, dataIni, dataFim) {
-  return {
-    "channel": "MOBILE",
-    "capture": "PRESENTIAL_CAPTURE",
-    "paymentMethod": "CREDIT_CARD",
-    "paymentProcessor": "OTHERS",
-    "startedAt": dataIni,
-    "endedAt": dataFim,
-    "escrow": {
-      "type": "UNIQUE",
-      "value": value
-    },
-    "costs": [
+    "name": faker.animal.petName(),
+    "photoUrls": [
+      "string"
+    ],
+    "tags": [
       {
-        "type": "INTERMEDIATION_RATE",
-        "installments": [
-          {
-            "min": 1,
-            "max": 1,
-            "value": 1.99
-          },
-          {
-            "min": 2,
-            "max": 18,
-            "value": 1.99
-          }
-        ]
-      },
-      {
-        "type": "INTERMEDIATION_FEE",
-        "installments": [
-          {
-            "min": 1,
-            "max": 1,
-            "value": 0
-          },
-          {
-            "min": 2,
-            "max": 18,
-            "value": 0
-          }
-        ]
+        "id": 1,
+        "name": faker.animal.dog()
       }
-    ]
-  }
-}
-
-export function bodyForcaPayment(paymentId) {
-  return JSON.stringify({
-    "type": "exec",
-    "mbean": "ps.bko.unit.jmx:name=paymentJmx,type=PaymentJmx",
-    "operation": "reprocessPaymentSyncByPaymentCode(java.lang.String)",
-    "arguments": [
-      paymentId
-    ]
-  })
-}
-
-export function bodySuspensao(paymentId){
-  return JSON.stringify({
-    "created_at": novaData.toISOString().substring(0, 19)+"-03:00",
-    "payment_code": paymentId,
-    "reason": "CHARGEBACK",
-    "reference_code": novoUUID
-  })
-}
-
-export function bodyRemoveSuspensao(valorCancelado){
-  return JSON.stringify({
-    "reasons": [
-      {
-        "canceled_amount": valorCancelado,
-        "created_at": novaData.toISOString().substring(0, 19)+"-03:00",
-        "order": 1,
-        "reason": "CHARGEBACK"
-      }
-    ]
-  })
-}
-
-export function bodyAntecipacao(codCustomer,agenda){
-  if(Array.isArray(agenda)){
-    var fee_amount = agenda.reduce((arr, x) => {return arr+x.fee_amount},0)
-    var gross_amount = agenda.reduce((arr, x) => {return arr+x.gross_amount},0)
-    var dates = agenda.reduce((arr, x) => arr.concat(x.due_date.toString()),[])
-  }else{
-    var fee_amount = agenda.fee_amount
-    var gross_amount = agenda.gross_amount
-    var dates = new Array(agenda.due_date)
-  }
-
-  return JSON.stringify({
-    "origin": "DESK",
-    "anticipation_code": novoUUID,
-    "guarantees_due_dates": dates,
-    "anticipation_type": "CLIENT",
-    "expected_amounts": {
-        "fee_amount": fee_amount.toString(),
-        "gross_amount": gross_amount.toString()
-    },
-    "customer_code": codCustomer
+    ],
+    "status": "available"
   })
 }
